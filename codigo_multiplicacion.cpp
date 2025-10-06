48eb8689174feffb3b97c9ea07b057f589e63b62
Macias10 <mlmacias@alumno.montepiedra.edu.ec>
2025-10-06 09:08:52 -0500

    Add files via upload

---
+++ codigo_multiplicacion.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Declaración de variables
+    float num1, num2, resultado;
+
+    // Entrada de datos
+    cout << "Ingrese el primer número: ";
+    cin >> num1;
+    cout << "Ingrese el segundo número: ";
+    cin >> num2;
+
+    // Proceso: multiplicación
+    resultado = num1 * num2;
+
+    // Salida de resultado
+    cout << "El resultado de la multiplicación es: " << resultado << endl;
+
+    return 0;
+}
